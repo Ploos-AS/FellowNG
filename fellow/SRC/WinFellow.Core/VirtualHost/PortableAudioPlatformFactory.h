354af64053a444974ee3e85186782ea3db0b1f5a
pos <pgousdal@users.noreply.github.com>
2026-09-14 15:21:46 +0200

    M4.5d4: add portable audio platform factory decorator

---
+++ fellow/SRC/WinFellow.Core/VirtualHost/PortableAudioPlatformFactory.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Driver/PortableSoundDriver.h"
+#include "Platform/IAudioOutput.h"
+#include "VirtualHost/ICorePlatformFactory.h"
+
+class PortableAudioPlatformFactory final : public ICorePlatformFactory
+{
+public:
+  PortableAudioPlatformFactory(ICorePlatformFactory &fallback, Platform::IAudioOutput &audio_output)
+    : _fallback(fallback), _audio_output(audio_output)
+  {
+  }
+
+  ISoundDriver *CreateSoundDriver() override
+  {
+    return new FellowNG::Driver::PortableSoundDriver(_audio_output);
+  }
+
+  Service::IFileops *CreateFileops(Service::ILog *log) override
+  {
+    return _fallback.CreateFileops(log);
+  }
+
+  Service::IHud *CreateHud() override
+  {
+    return _fallback.CreateHud();
+  }
+
+  Service::IRetroPlatform *CreateRetroPlatform() override
+  {
+    return _fallback.CreateRetroPlatform();
+  }
+
+private:
+  ICorePlatformFactory &_fallback;
+  Platform::IAudioOutput &_audio_output;
+};
