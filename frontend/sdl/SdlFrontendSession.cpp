88fa29bd8622add9de1ecea0ad9ebe1573498df4
pos <pgousdal@users.noreply.github.com>
2026-09-14 22:29:28 +0200

    M4.5d8: construct SDL session through runtime factory

--- frontend/sdl/SdlFrontendSession.cpp
+++ frontend/sdl/SdlFrontendSession.cpp
@@ -6,27 +6,46 @@ SdlFrontendSession::SdlFrontendSession(Platform::IEmulatorRuntime &runtime,
                                        Platform::IInputSource &input,
                                        Platform::IVideoOutput &video,
                                        Platform::IAudioOutput &audio)
-    : _session(runtime, input, video, audio)
+    : _borrowed_session(std::make_unique<Platform::FrontendSession>(runtime, input, video, audio)),
+      _session(_borrowed_session.get())
 {
+}
+
+SdlFrontendSession::SdlFrontendSession(Platform::IEmulatorRuntimeFactory &runtime_factory,
+                                       Platform::IInputSource &input,
+                                       Platform::IVideoOutput &video,
+                                       Platform::IAudioOutput &audio)
+    : _owned_runtime(runtime_factory.Create())
+{
+  if (_owned_runtime)
+  {
+    _owned_session = std::make_unique<Platform::FrontendSession>(*_owned_runtime, input, video, audio);
+    _session = _owned_session.get();
+  }
 }
 
 bool SdlFrontendSession::Start()
 {
-  return _session.Start();
+  return _session != nullptr && _session->Start();
 }
 
 bool SdlFrontendSession::PumpOnce()
 {
-  return _session.PumpOnce();
+  return _session != nullptr && _session->PumpOnce();
 }
 
 void SdlFrontendSession::Stop()
 {
-  _session.Stop();
+  if (_session != nullptr) _session->Stop();
 }
 
 bool SdlFrontendSession::IsRunning() const
 {
-  return _session.IsRunning();
+  return _session != nullptr && _session->IsRunning();
+}
+
+bool SdlFrontendSession::HasRuntime() const
+{
+  return _session != nullptr;
 }
 }
