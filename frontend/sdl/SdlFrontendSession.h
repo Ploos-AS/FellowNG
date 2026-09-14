5e6620540cd931c92e27ca1fb89aafd71b05f100
pos <pgousdal@users.noreply.github.com>
2026-09-14 22:29:18 +0200

    M4.5d8: let SDL session own factory-created runtime

--- frontend/sdl/SdlFrontendSession.h
+++ frontend/sdl/SdlFrontendSession.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <memory>
+
 #include "Platform/FrontendSession.h"
+#include "Platform/IEmulatorRuntimeFactory.h"
 
 namespace FellowNG::Frontend::SDL
 {
@@ -8,12 +11,21 @@ class SdlFrontendSession
 {
 public:
   SdlFrontendSession(Platform::IEmulatorRuntime &runtime, Platform::IInputSource &input, Platform::IVideoOutput &video, Platform::IAudioOutput &audio);
+  SdlFrontendSession(Platform::IEmulatorRuntimeFactory &runtime_factory,
+                     Platform::IInputSource &input,
+                     Platform::IVideoOutput &video,
+                     Platform::IAudioOutput &audio);
+
   bool Start();
   bool PumpOnce();
   void Stop();
   bool IsRunning() const;
+  bool HasRuntime() const;
 
 private:
-  Platform::FrontendSession _session;
+  std::unique_ptr<Platform::IEmulatorRuntime> _owned_runtime;
+  std::unique_ptr<Platform::FrontendSession> _owned_session;
+  Platform::FrontendSession *_session = nullptr;
+  std::unique_ptr<Platform::FrontendSession> _borrowed_session;
 };
 }
