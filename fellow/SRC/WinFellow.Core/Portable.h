128c11fe3b34aa7febbc2469faad41956467abe4
pos <pgousdal@users.noreply.github.com>
2026-09-15 06:55:33 +0200

    M4.5d12: make Portable.h host-portable

--- fellow/SRC/WinFellow.Core/Portable.h
+++ fellow/SRC/WinFellow.Core/Portable.h
@@ -2,45 +2,26 @@
 
 /*====================================================*/
 /* Wrapper definitions for system dependent functions */
-/* Windows, MSVC++                                    */
 /*====================================================*/
 
+#if defined(_WIN32)
+
+/* Windows, MSVC++ */
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <windows.h>
-
-/*=======*/
-/* stdio */
-/*=======*/
-
 #include <stdio.h>
 #include <io.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <memory.h>
 
 #define fileno _fileno
 #define access _access
-
-/*================================*/
-/* stat structure and field names */
-/*================================*/
-
-#include <sys/stat.h>
-
-/*=====================*/
-/* string manipulation */
-/*=====================*/
-
-#include <string.h>
-
 #define strcmpi _strcmpi
 #define stricmp _stricmp
 #define strnicmp _strnicmp
 #define strlwr _strlwr
-
-/*====================================*/
-/* memory manipulation and allocation */
-/*====================================*/
-
-#include <memory.h>
 
 #ifdef _FELLOW_DEBUG_CRT_MALLOC
 #define _CRTDBG_MAP_ALLOC
@@ -50,9 +31,24 @@
 
 #ifdef _FELLOW_DEBUG_CRT_MALLOC
 #include <crtdbg.h>
-
 #define DBG_NEW new (_NORMAL_BLOCK, __FILE__, __LINE__)
 #define new DBG_NEW
+#endif
+
+#else
+
+/* POSIX-compatible hosts */
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <memory.h>
+#include <strings.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define strcmpi strcasecmp
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
 
 #endif
 
