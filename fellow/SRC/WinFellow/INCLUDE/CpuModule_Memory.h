ed2f409cc6787c229af9b93ba9fb9d315c18bb38
pos <pgousdal@users.noreply.github.com>
2026-09-17 15:13:37 +0200

    M4.5d28: isolate UAE filesystem trap headers from Linux CPU core

--- fellow/SRC/WinFellow/INCLUDE/CpuModule_Memory.h
+++ fellow/SRC/WinFellow/INCLUDE/CpuModule_Memory.h
@@ -12,8 +12,13 @@
 
 #include "MemoryInterface.h"
 
-// For trapping into the "UAE filesystem" driver
+// The legacy UAE filesystem trap integration is part of the WinFellow host
+// layer and its headers are not present in the portable Linux build. Keep
+// those dependencies on Windows while the filesystem trap is moved behind a
+// frontend-neutral host interface.
+#ifdef _WIN32
 #include "uae2fell.h"
 #include "autoconf.h"
+#endif
 
 #endif
