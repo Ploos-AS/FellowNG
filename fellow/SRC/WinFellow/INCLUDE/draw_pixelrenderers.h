d4bedf28c873b41aaffeb052dfce1c04e3f04320
pos <pgousdal@users.noreply.github.com>
2026-09-17 13:38:33 +0200

    M4.5d20: expose pixel renderer interfaces

--- fellow/SRC/WinFellow/INCLUDE/draw_pixelrenderers.h
+++ fellow/SRC/WinFellow/INCLUDE/draw_pixelrenderers.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <cstdint>
+
+// Pixel-renderer tables shared with Renderer.cpp and the graphics pipeline.
+extern uint8_t draw_dual_translate[2][256][256];
+extern uint32_t draw_HAM_modify_table[4][2];
+extern const uint32_t draw_HAM_modify_table_bitindex;
+extern const uint32_t draw_HAM_modify_table_holdmask;
+
 void drawDualTranslationInitialize();
 void drawHAMTableInit();
 void drawWriteProfilingResultsToFile();
