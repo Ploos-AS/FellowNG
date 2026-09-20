bb15ebc54a00c9b44fba5daa85628b3fbd121577
pos <pgousdal@users.noreply.github.com>
2026-09-20 15:10:38 +0200

    M6.1h: detach portable Windows startup from legacy sysinfo

--- fellow/SRC/WinFellow/C/FellowMain.cpp
+++ fellow/SRC/WinFellow/C/FellowMain.cpp
@@ -489,7 +489,11 @@ static void fellowModulesStartupCommon(
   CoreFactory::CreateServices();
 
 #if !defined(FELLOWNG_PORTABLE_FRONTEND)
+#if defined(_WIN32) && defined(FELLOWNG_PORTABLE_FRONTEND)
+  // Legacy Windows sysinfo belongs to the Win32 frontend; portable SDL avoids it.
+#else
   sysinfoLogSysInfo();
+#endif
 #endif
 
   CoreFactory::CreateDrivers();
